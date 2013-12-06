9a80d3668ed86867d9a2a9cbae2935ef493291de
Stephan Kuschel <stephan.kuschel@gmail.com>
2013-12-06 03:03:06 +0100

    SDFAnalyzer Datenzugriff vervollstaendigt

--- v1p1/__init__.py
+++ v1p1/__init__.py
@@ -37,7 +37,79 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import LinearSegmentedColormap
 
 
+class _Constants:
+    """
+    Stellt Konstatanten fuer dieses Modul bereit.
+    """
+    _c = 299792458.0
+    _me = 9.109383e-31
+    _qe = 1.602176565e-19
+    _mu0 = np.pi*4e-7 #N/A^2
+    _epsilon0 = 1 / (_mu0 * _c**2) #8.85419e-12 As/Vm
 
+    _axisoptions = {'X':slice(0,2), 'Y':slice(2,4), 'Z':slice(4,6), None:slice(None),
+            'x':slice(0,2), 'y':slice(2,4), 'z':slice(4,6)}    
+    _axisoptionseinzel = {'X':slice(0,1), 'Y':slice(1,2), 'Z':slice(2,3), None:slice(None), 'x':slice(0,1), 'y':slice(1,2), 'z':slice(2,3)} 
+    _axesidentify = {'X':0, 'x':0, 0:0,'Y':1, 'y':1, 1:1,'Z':2, 'z':2, 2:2}   
+    _poptsidentify = _axesidentify.copy()
+    _poptsidentify.update({'px':3,'Px':3,'py':4,'Py':4,'pz':5,'Pz':5,'weight':9,'w':9})
+
+    @staticmethod
+    def ncrit_um(lambda_um):
+        return 1.11e27*1/(lambda_um**2) #1/m^3
+
+    @staticmethod
+    def ncrit(laslambda):
+        return _Constants.ncrit_um(laslambda * 1e6) #1/m^3
+        
+    @staticmethod
+    def datenausschnitt(m, oldextent, newextent):
+        """
+        Schneidet aus einer Datenmatrix m den Teil heraus, der newextent entspricht, wenn die gesamte Matrix zuvor oldextent entsprochen hat. Hat m insgeamt dims dimensionen, dann muessen oldextent und newextent die Laenge 2*dims haben.
+        Einschraenkung: newextent muss innerhalb von oldextent liegen
+        
+        """
+        dims = len(m.shape)
+        assert not oldextent is newextent, 'oldextent und newextent zeigen auf dasselbe Objekt(!). Da hat wohl jemand beim Programmieren geschlafen :)'
+        assert len(oldextent) / 2 == dims, 'Dimensionen von m und oldextent falsch!'
+        assert len(newextent) / 2 == dims, 'Dimensionen von m und newextent falsch!'
+        s=()
+        for dim in range(dims):
+            i=2*dim
+            thisdimmin = round((newextent[i]-oldextent[i])/(oldextent[i + 1]-oldextent[i])*m.shape[dim])
+            thisdimmax = round((newextent[i + 1]-oldextent[i])/(oldextent[i + 1]-oldextent[i])*m.shape[dim])
+            s = np.append(s, slice(thisdimmin, thisdimmax))
+        if len(s) == 1:
+            s=s[0]
+        else:
+            s=tuple(s)
+        return m[s]
+
+    # Transformation: polarkoordinaten --> kartesische Koordinaten,
+    # (i,j), Indizes des Bildes in Polardarstellung --> Indizes des Bildes in kartesischer Darstellung
+    # damit das Bild transformiert wird: kartesischen Koordinaten --> polarkoordinaten
+    @staticmethod
+    def transfromxy2polar(matrixxy, extentxy, extentpolar, shapepolar, ashistogram=True):
+        """transformiert eine Matrix in kartesischer Darstellung (matrixxy, Achsen x,y) in eine Matrix in polardarstellung (Achsen r,phi)."""
+        from scipy import ndimage
+        def polar2xy((r, phi)):
+            x = r * np.cos(phi)
+            y = r * np.sin(phi)
+            return (x,y)
+        def koord2index((q1,q2), extent, shape):
+            return ( (q1-extent[0])/(extent[1]-extent[0])*shape[0], (q2-extent[2])/(extent[3]-extent[2])*shape[1] )
+        def index2koord((i,j), extent, shape):
+            return ( extent[0] + i/shape[0] * (extent[1]-extent[0]), extent[2] + j/shape[1] * (extent[3]-extent[2]) )
+        def mappingxy2polar((i,j), extentxy, shapexy, extentpolar, shapepolar):
+            """actually maps indizes of polar matrix to indices of kartesian matrix"""
+            ret = polar2xy(index2koord((float(i), float(j)), extentpolar, shapepolar))
+            ret = koord2index(ret, extentxy, shapexy)
+            return ret
+        ret = ndimage.interpolation.geometric_transform(matrixxy, mappingxy2polar, output_shape=shapepolar, extra_arguments=(extentxy, matrixxy.shape, extentpolar, shapepolar), order=1)
+        if ashistogram: #Volumenelement ist einfach nur r
+            ret = (ret.T * np.abs(np.linspace(extentpolar[0], extentpolar[1], ret.shape[0]))).T
+        return ret
+        
 
 class PlotDescriptor():
     """
@@ -175,7 +247,7 @@ class OutputAnalyzer(PlotDescriptor):
         return self._createtimeseries_simple(f)
 
 
-class SDFAnalyzer(PlotDescriptor):
+class SDFAnalyzer(PlotDescriptor, _Constants):
     """
     Liest die sdfdatei ein und gibt und berechnet grundlegende Informationen.
     """
@@ -185,18 +257,25 @@ class SDFAnalyzer(PlotDescriptor):
             print("-------------- " + dateiname + " --------------")
         import sdf
         self.dateiname = dateiname
-        self.data = sdf.SDF(dateiname).read()
+        self._data = sdf.SDF(dateiname).read()
         self.dumpname = os.path.basename(dateiname).replace('.sdf','')
         self.projektname = os.path.basename(os.getcwd())
-        self.header = self.data['Header']
+        self.header = self._data['Header']
         self.simdimensions = float(re.match('Epoch(\d)d', self.header['code_name']).group(1))
         self.lasnm = lasnm
+        #Simextent bestimmen
+        self._simextent = np.real([self._data['Grid/Grid_node/X'][0], self._data['Grid/Grid_node/X'][-1]])
+        if self.simdimensions > 1:
+            self._simextent = np.append(self._simextent, np.float64([self._data['Grid/Grid_node/Y'][0], self._data['Grid/Grid_node/Y'][-1]]))
+        if self.simdimensions > 2:
+            self._simextent = np.append(self._simextent, np.float64([self._data['Grid/Grid_node/Z'][0], self._data['Grid/Grid_node/Z'][-1]]))
         if lasnm:
             if printinfo:
                 print "lambda_0 (nm) = " + str(lasnm)
                 print self.species(ejected='all')
         else:
             print "WARNING: Laserwellenlaenge nicht gegeben. Einige Plots stehen nicht zur Verfuegung."
+            
 
         
     def __str__(self):
@@ -231,7 +310,7 @@ class SDFAnalyzer(PlotDescriptor):
             raise Exception('Unit ' + str(unit) + ' unbekannt')
             
             
-
+   #High Level
     def species(self, ejected='ignore'):
         """
         Gibt eine Liste aller an der Simulation beteiligten Species aus
@@ -241,13 +320,16 @@ class SDFAnalyzer(PlotDescriptor):
         'ignore' - ejected particles werden nicht mit ausgegeben.
         """
         ret = []
-        for key in self.data.keys():
+        for key in self._data.keys():
             match = re.match('Particles/Px(/\w+)', key)
             if match:
                 if ejected=='all' or (ParticleAnalyzer.isejected(match.group(1)) == (ejected=='only')):
                     ret = np.append(ret, match.group(1))
         ret.sort()
         return ret
+        
+    def hasSpecies(self, species):
+        return species in self.species(ejected='all')
      
     def ions(self, ejected='ignore'):
         """
@@ -278,7 +360,7 @@ class SDFAnalyzer(PlotDescriptor):
         return ret
 
     def getfieldanalyzer(self):
-        return FieldAnalyzer(self.data, lasnm=self.lasnm)
+        return FieldAnalyzer(self._data, lasnm=self.lasnm)
 
     def getparticleanalyzer(self, species, ejected='ignore'):
         """
@@ -289,98 +371,67 @@ class SDFAnalyzer(PlotDescriptor):
         'ignore' - ejected particles werden nicht mit ausgegeben.
         """
         if species == 'ions':
-            return ParticleAnalyzer(self.data, *self.ions(ejected=ejected))
+            return ParticleAnalyzer(self._data, *self.ions(ejected=ejected))
         elif species == 'nonions':
-            return ParticleAnalyzer(self.data, *self.nonions(ejected=ejected))
+            return ParticleAnalyzer(self._data, *self.nonions(ejected=ejected))
         else:
-            return ParticleAnalyzer(self.data, species)
+            return ParticleAnalyzer(self._data, species)
 
-    ### TODO: Passt hier eigntlich nicht so richtig hin, weil der SDFAnalyzer die einzige Klasse ist, die direkt mit den dem data objekt aus dem .sdf kommunizieren soll. 
+   #low-level
     def getderived(self):
-        """Gibt alle Keys zurueck die mit "Derived/" beginnen"""
+        """Gibt alle Keys zurueck die mit "Derived/" beginnen. Diese sollten direkt an data(key) weitergegeben werden koennen, um die Daten zu erhalten."""
         ret = []
-        for key in self.data.keys():
+        for key in self._data.keys():
             r = re.match('Derived/[\w/ ]*', key)
             if r:
                 ret.append(r.group(0))
         ret.sort()
         return ret
+    
+    def data(self,key):
+        """Gibt die Daten zu diesem Key zurueck (in float64)"""
+        assert self._data.has_key(key), 'Den Key ' + key + ' gibts nicht!'
+        return np.float64(self._data[key])
 
-
-
-
-
-
-class _Constants:
-    """
-    Stellt Konstatanten fuer dieses Modul bereit.
-    """
-    _c = 299792458.0
-    _me = 9.109383e-31
-    _qe = 1.602176565e-19
-    _mu0 = np.pi*4e-7 #N/A^2
-    _epsilon0 = 1 / (_mu0 * _c**2) #8.85419e-12 As/Vm
-
-    _axisoptions = {'X':slice(0,2), 'Y':slice(2,4), 'Z':slice(4,6), None:slice(None),
-            'x':slice(0,2), 'y':slice(2,4), 'z':slice(4,6)}    
-    _axisoptionseinzel = {'X':slice(0,1), 'Y':slice(1,2), 'Z':slice(2,3), None:slice(None), 'x':slice(0,1), 'y':slice(1,2), 'z':slice(2,3)}    
-
-    @staticmethod
-    def ncrit_um(lambda_um):
-        return 1.11e27*1/(lambda_um**2) #1/m^3
-
-    @staticmethod
-    def ncrit(laslambda):
-        return _Constants.ncrit_um(laslambda * 1e6) #1/m^3
+    def _returnkey2(self, key1, key2, average=False):
+        key = key1 + key2
+        if average:
+            key = key1 + '_average' + key2
+        return self.data(key)
         
-    @staticmethod
-    def datenausschnitt(m, oldextent, newextent):
-        """
-        Schneidet aus einer Datenmatrix m den Teil heraus, der newextent entspricht, wenn die gesamte Matrix zuvor oldextent entsprochen hat. Hat m insgeamt dims dimensionen, dann muessen oldextent und newextent die Laenge 2*dims haben.
-        Einschraenkung: newextent muss innerhalb von oldextent liegen
+    def dataE(self, axis, **kwargs):
+        axsuffix = {0:'x',1:'y',2:'z'}[_Constants._axesidentify[axis]]
+        return self._returnkey2('Electric Field', '/E' + axsuffix, **kwargs)
         
-        """
-        dims = len(m.shape)
-        assert not oldextent is newextent, 'oldextent und newextent zeigen auf dasselbe Objekt(!). Da hat wohl jemand beim Programmieren geschlafen :)'
-        assert len(oldextent) / 2 == dims, 'Dimensionen von m und oldextent falsch!'
-        assert len(newextent) / 2 == dims, 'Dimensionen von m und newextent falsch!'
-        s=()
-        for dim in range(dims):
-            i=2*dim
-            thisdimmin = round((newextent[i]-oldextent[i])/(oldextent[i + 1]-oldextent[i])*m.shape[dim])
-            thisdimmax = round((newextent[i + 1]-oldextent[i])/(oldextent[i + 1]-oldextent[i])*m.shape[dim])
-            s = np.append(s, slice(thisdimmin, thisdimmax))
-        if len(s) == 1:
-            s=s[0]
-        else:
-            s=tuple(s)
-        return m[s]
+    def dataB(self, axis, **kwargs):
+        axsuffix = {0:'x',1:'y',2:'z'}[_Constants._axesidentify[axis]]
+        return self._returnkey2('Magnetic Field', '/B' + axsuffix, **kwargs)
 
-    # Transformation: polarkoordinaten --> kartesische Koordinaten,
-    # (i,j), Indizes des Bildes in Polardarstellung --> Indizes des Bildes in kartesischer Darstellung
-    # damit das Bild transformiert wird: kartesischen Koordinaten --> polarkoordinaten
-    @staticmethod
-    def transfromxy2polar(matrixxy, extentxy, extentpolar, shapepolar, ashistogram=True):
-        """transformiert eine Matrix in kartesischer Darstellung (matrixxy, Achsen x,y) in eine Matrix in polardarstellung (Achsen r,phi)."""
-        from scipy import ndimage
-        def polar2xy((r, phi)):
-            x = r * np.cos(phi)
-            y = r * np.sin(phi)
-            return (x,y)
-        def koord2index((q1,q2), extent, shape):
-            return ( (q1-extent[0])/(extent[1]-extent[0])*shape[0], (q2-extent[2])/(extent[3]-extent[2])*shape[1] )
-        def index2koord((i,j), extent, shape):
-            return ( extent[0] + i/shape[0] * (extent[1]-extent[0]), extent[2] + j/shape[1] * (extent[3]-extent[2]) )
-        def mappingxy2polar((i,j), extentxy, shapexy, extentpolar, shapepolar):
-            """actually maps indizes of polar matrix to indices of kartesian matrix"""
-            ret = polar2xy(index2koord((float(i), float(j)), extentpolar, shapepolar))
-            ret = koord2index(ret, extentxy, shapexy)
-            return ret
-        ret = ndimage.interpolation.geometric_transform(matrixxy, mappingxy2polar, output_shape=shapepolar, extra_arguments=(extentxy, matrixxy.shape, extentpolar, shapepolar), order=1)
-        if ashistogram: #Volumenelement ist einfach nur r
-            ret = (ret.T * np.abs(np.linspace(extentpolar[0], extentpolar[1], ret.shape[0]))).T
-        return ret
-        
+
+    def simextent(self):
+        return self._simextent
+       
+    def grid(self, axis):
+        """axis wird erkannt sofern es in _Constants._axesidentify ist"""
+        axsuffix = {0:'X',1:'Y',2:'Z'}[_Constants._axesidentify[axis]]
+        return self._data['Grid/Grid/' + axsuffix]
+
+    def grid_node(self, axis):
+        """axis wird erkannt sofern es in _Constants._axesidentify ist"""
+        axsuffix = {0:'X',1:'Y',2:'Z'}[_Constants._axesidentify[axis]]
+        return self._data['Grid/Grid_node/' + axsuffix]
+
+    def getSpecies(self, spezies, attrib):
+        """Gibt das Attribut (x,y,z,px,py,pz,weight) dieser Teilchenspezies zurueck"""
+        attribid = _Constants._poptsidentify[attrib]
+        options={9:lambda s: 'Particles/Weight' + s,
+            0:lambda s: 'Grid/Particles' + s + '/X',
+            1:lambda s: 'Grid/Particles' + s + '/Y',
+            2:lambda s: 'Grid/Particles' + s + '/Z',
+            3:lambda s: 'Particles/Px' + s,
+            4:lambda s: 'Particles/Py' + s,
+            5:lambda s: 'Particles/Pz' + s}
+        return self._data[options[attribid](spezies)]
 
         
 
