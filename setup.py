e4227d518a47b232cb4233809b2d24f608e0336c
Stephan Kuschel <stephan.kuschel@gmail.com>
2013-12-06 15:03:39 +0100

    setup.py erweitert.

--- setup.py
+++ setup.py
@@ -3,10 +3,13 @@
 import os
 from setuptools import setup
 
-version=0.1
+
 
 setup(name='epochsdftools',
+    version='0.1'
 	author='Stephan Kuschel',
+	author_email='stephan.kuschel@uni-jena.de',
+	description='Toolkit um Daten aus PIC Simulationen zu verarbeiten und darzustellen.',
 	packages=['epochsdftools','epochsdftools.v1p1'],
 	install_requires=['matplotlib']
 	)
