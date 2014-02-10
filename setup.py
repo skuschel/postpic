d34203b4b1a40f8901e3aa397c0f5b37bdc3cc94
Stephan Kuschel <stephan.kuschel@gmail.com>
2014-02-10 12:11:17 +0100

    changed directory structure. updated description.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python2
 
 from setuptools import setup
 from epochsdftools import __version__
@@ -7,8 +7,8 @@ from epochsdftools import __version__
 setup(name='epochsdftools',
     version=__version__,
 	author='Stephan Kuschel',
-	author_email='stephan.kuschel@uni-jena.de',
-	description='Toolkit um Daten aus PIC Simulationen zu verarbeiten und darzustellen.',
+	author_email='stephan.kuschel@gmail.de',
+	description='Provides easy-to-use functions to create customized plots from simulations data created by the epoch code. This toolset can be easily adapted to also support the output of other particle in cell code.',
 	packages=['epochsdftools'],
 	install_requires=['matplotlib', 'numpy', 'scipy']
 	)
