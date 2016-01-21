3f8a3ca85e29ed17b5306466d415da934508f597
Stephan Kuschel <stephan.kuschel@gmail.com>
2016-01-21 11:15:55 +0100

    pythonify examples: add main function

--- examples/openPMD.py
+++ examples/openPMD.py
@@ -18,8 +18,6 @@
 # Copyright Stephan Kuschel 2016
 #
 
-import os
-
 def download(url, file):
     import urllib3
     import shutil
@@ -32,90 +30,94 @@ def download(url, file):
     with http.request('GET', url, preload_content=False) as r, open(file, 'wb') as out_file:
         shutil.copyfileobj(r, out_file)
 
-if not os.path.exists('examples/_openPMDdata'):
-    os.mkdir('examples/_openPMDdata')
-download('https://github.com/openPMD/openPMD-example-datasets/'
-       + 'raw/776ae3a96c02b20cfae56efafcbda6ca76d4c78d/example-2d.tar.gz', 
-         'examples/_openPMDdata/example-2d.tar.gz')
+def main():
+    import os
+    if not os.path.exists('examples/_openPMDdata'):
+        os.mkdir('examples/_openPMDdata')
+    download('https://github.com/openPMD/openPMD-example-datasets/'
+           + 'raw/776ae3a96c02b20cfae56efafcbda6ca76d4c78d/example-2d.tar.gz',
+             'examples/_openPMDdata/example-2d.tar.gz')
 
-import tarfile
-tar = tarfile.open('examples/_openPMDdata/example-2d.tar.gz')
-tar.extractall('examples/_openPMDdata')
-
-
-# now that files are downloaded and extracted, start data evaluation
-
-import numpy as np
-import postpic as pp
-
-# postpic will use matplotlib for plotting. Changing matplotlibs backend
-# to "Agg" makes it possible to save plots without a display attached.
-# This is necessary to run this example within the "run-tests" script
-# on travis-ci.
-import matplotlib; matplotlib.use('Agg')
-pp.chooseCode('openpmd')
-dr = pp.readDump('examples/_openPMDdata/example-2d/hdf5/data00000300.h5')
-# set and create directory for pictures.
-savedir = '_examplepictures/openPMD/'
-import os
-if not os.path.exists(savedir):
-    os.mkdir(savedir)
-
-# initialze the plotter object.
-# project name will be prepended to all output names
-plotter = pp.plotting.plottercls(dr, outdir=savedir, autosave=True, project='OpenPMD')
-
-# we will need a refrence to the MultiSpecies quite often
-from postpic import MultiSpecies as MS
-
-# create MultiSpecies Object for every particle species that exists.
-pas = [MS(dr, s) for s in dr.listSpecies()]
-
-if True:
-    # Plot Data from the FieldAnalyzer fa. This is very simple: every line creates one plot
-    plotter.plotField(dr.Ex())  # plot 0
-    plotter.plotField(dr.Ey())  # plot 1
-    plotter.plotField(dr.Ez())  # plot 2
-    plotter.plotField(dr.energydensityEM())  # plot 3
-
-    # Using the MultiSpecies requires an additional step:
-    # 1) The MultiSpecies.createField method will be used to create a Field object
-    # with choosen particle scalars on every axis
-    # 2) Plot the Field object
-    optargsh={'bins': [200,50]}
-    for pa in pas:
-        # Remark on 2D or 3D Simulation:
-        # the data fields for the plots in the following section are build by postpic
-        # only from the particle data. The following plots would just the same,
-        # if we would use the data of a 3D Simulation instead.
-
-        # create a Field object nd holding the number density
-        nd = pa.createField(MS.Z, MS.X, optargsh=optargsh,simextent=False)
-        # plot the Field object nd
-        plotter.plotField(nd, name='NumberDensity')   # plot 4
-        # more advanced: create a field holding the total kinetic energy on grid
-        ekin = pa.createField(MS.Z, MS.X, weights=MS.Ekin_MeV, optargsh=optargsh, simextent=False)
-        # The Field objectes can be used for calculations. Here we use this to
-        # calculate the average kinetic energy on grid and plot
-        plotter.plotField(ekin / nd, name='Avg Kin Energy (MeV)')  # plot 5
-
-        # use optargsh to force lower resolution
-        # plot number density
-        plotter.plotField(pa.createField(MS.Z, MS.X, optargsh=optargsh), lineoutx=True, lineouty=True)  # plot 6
-        # plot phase space
-        plotter.plotField(pa.createField(MS.Z, MS.P, optargsh=optargsh))  # plot 7
-        plotter.plotField(pa.createField(MS.Z, MS.gamma, optargsh=optargsh))  # plot 8
-        plotter.plotField(pa.createField(MS.Z, MS.beta, optargsh=optargsh))  # plot 9
+    import tarfile
+    tar = tarfile.open('examples/_openPMDdata/example-2d.tar.gz')
+    tar.extractall('examples/_openPMDdata')
 
 
-if True:
-    # instead of reading a single dump read a collection of dumps using the simulationreader
-    sr = pp.readSim('examples/_openPMDdata/example-2d/hdf5/*.h5')
-    print('There are {:} dumps in this simulationreader object:'.format(len(sr)))
-    # now you can iterate over the dumps written easily
-    for dr in sr:
-        print('Simulation time of current dump t = {:.2e} s'.format(dr.time()))
-        # there are the frames of a movie:
-        plotter.plotField(dr.Ez())
+    # now that files are downloaded and extracted, start data evaluation
 
+    import numpy as np
+    import postpic as pp
+
+    # postpic will use matplotlib for plotting. Changing matplotlibs backend
+    # to "Agg" makes it possible to save plots without a display attached.
+    # This is necessary to run this example within the "run-tests" script
+    # on travis-ci.
+    import matplotlib; matplotlib.use('Agg')
+    pp.chooseCode('openpmd')
+    dr = pp.readDump('examples/_openPMDdata/example-2d/hdf5/data00000300.h5')
+    # set and create directory for pictures.
+    savedir = '_examplepictures/openPMD/'
+    import os
+    if not os.path.exists(savedir):
+        os.mkdir(savedir)
+
+    # initialze the plotter object.
+    # project name will be prepended to all output names
+    plotter = pp.plotting.plottercls(dr, outdir=savedir, autosave=True, project='OpenPMD')
+
+    # we will need a refrence to the MultiSpecies quite often
+    from postpic import MultiSpecies as MS
+
+    # create MultiSpecies Object for every particle species that exists.
+    pas = [MS(dr, s) for s in dr.listSpecies()]
+
+    if True:
+        # Plot Data from the FieldAnalyzer fa. This is very simple: every line creates one plot
+        plotter.plotField(dr.Ex())  # plot 0
+        plotter.plotField(dr.Ey())  # plot 1
+        plotter.plotField(dr.Ez())  # plot 2
+        plotter.plotField(dr.energydensityEM())  # plot 3
+
+        # Using the MultiSpecies requires an additional step:
+        # 1) The MultiSpecies.createField method will be used to create a Field object
+        # with choosen particle scalars on every axis
+        # 2) Plot the Field object
+        optargsh={'bins': [200,50]}
+        for pa in pas:
+            # Remark on 2D or 3D Simulation:
+            # the data fields for the plots in the following section are build by postpic
+            # only from the particle data. The following plots would just the same,
+            # if we would use the data of a 3D Simulation instead.
+
+            # create a Field object nd holding the number density
+            nd = pa.createField(MS.Z, MS.X, optargsh=optargsh,simextent=False)
+            # plot the Field object nd
+            plotter.plotField(nd, name='NumberDensity')   # plot 4
+            # more advanced: create a field holding the total kinetic energy on grid
+            ekin = pa.createField(MS.Z, MS.X, weights=MS.Ekin_MeV, optargsh=optargsh, simextent=False)
+            # The Field objectes can be used for calculations. Here we use this to
+            # calculate the average kinetic energy on grid and plot
+            plotter.plotField(ekin / nd, name='Avg Kin Energy (MeV)')  # plot 5
+
+            # use optargsh to force lower resolution
+            # plot number density
+            plotter.plotField(pa.createField(MS.Z, MS.X, optargsh=optargsh), lineoutx=True, lineouty=True)  # plot 6
+            # plot phase space
+            plotter.plotField(pa.createField(MS.Z, MS.P, optargsh=optargsh))  # plot 7
+            plotter.plotField(pa.createField(MS.Z, MS.gamma, optargsh=optargsh))  # plot 8
+            plotter.plotField(pa.createField(MS.Z, MS.beta, optargsh=optargsh))  # plot 9
+
+
+    if True:
+        # instead of reading a single dump read a collection of dumps using the simulationreader
+        sr = pp.readSim('examples/_openPMDdata/example-2d/hdf5/*.h5')
+        print('There are {:} dumps in this simulationreader object:'.format(len(sr)))
+        # now you can iterate over the dumps written easily
+        for dr in sr:
+            print('Simulation time of current dump t = {:.2e} s'.format(dr.time()))
+            # there are the frames of a movie:
+            plotter.plotField(dr.Ez())
+
+if __name__=='__main__':
+    main()
 
--- examples/particleshapedemo.py
+++ examples/particleshapedemo.py
@@ -23,106 +23,109 @@ This is a demonstration file to show the differences between various particles
 shapes used.
 '''
 
-import numpy as np
-import postpic as pp
+def main():
+    import numpy as np
+    import postpic as pp
 
-# postpic will use matplotlib for plotting. Changing matplotlibs backend
-# to "Agg" makes it possible to save plots without a display attached.
-# This is necessary to run this example within the "run-tests" script
-# on travis-ci.
-import matplotlib; matplotlib.use('Agg')
-
-
-# choose the dummy reader. This reader will create fake data for testing.
-pp.chooseCode('dummy')
-
-# Create a dummy reader with 300 particles, not initialized with a seed and use
-# uniform distribution
-dr = pp.readDump(300, seed=None, randfunc=np.random.random)
-# set and create directory for pictures.
-savedir = '_examplepictures/'
-import os
-if not os.path.exists(savedir):
-    os.mkdir(savedir)
-
-# initialze the plotter object.
-# project name will be prepended to all output names
-plotter = pp.plotting.plottercls(dr, outdir=savedir, autosave=True, project='particleshapedemo')
-
-# we will need a refrence to the MultiSpecies quite often
-from postpic import MultiSpecies as MS
-
-# create MultiSpecies object for every particle species that exists.
-pas = [MS(dr, s) for s in dr.listSpecies()]
-
-# --- 1D visualization of particle contributions ---
-
-def particleshapedemo(shape):
-    import postpic.cythonfunctions as cf
-    import matplotlib.pyplot as plt
-    ptclpos = np.array([4.5, 9.75, 15.0, 20.25])
-    y, edges = cf.histogram(ptclpos, bins=25, range=(0,25), shape=shape)
-    x = np.convolve(edges, [0.5, 0.5], mode='valid')
-    fig = plt.figure()
-    fig.suptitle('ParticleShape: {:s}'.format(str(shape)))
-    ax = fig.add_subplot(111)
-    ax.plot(x,y)
-    ax.set_ylim((0,1))
-    ax.set_xticks(x, minor=True)
-    ax.grid(which='minor')
-    for ix in ptclpos:
-        ax.axvline(x=ix, color='y')
-    fig.savefig(savedir + 'particleshapedemo{:s}.png'.format(str(shape)), dpi=160)
-    plt.close(fig)
-
-if True:
-    particleshapedemo(0)
-    particleshapedemo(1)
-    particleshapedemo(2)
-
-# --- 1D ---
-if True:
-        pa = pas[0]
-        plotargs = {'ylim': (0,1600), 'log10plot': False}
-
-        # 1 particle per cell
-        plotter.plotField(pa.createField(MS.X, optargsh={'bins': 300, 'shape': 0}, title='1ppc_order0', rangex=(0,1)), **plotargs)
-        plotter.plotField(pa.createField(MS.X, optargsh={'bins': 300, 'shape': 1}, title='1ppc_order1', rangex=(0,1)), **plotargs)
-        plotter.plotField(pa.createField(MS.X, optargsh={'bins': 300, 'shape': 2}, title='1ppc_order2', rangex=(0,1)), **plotargs)
-
-        # 3 particles per cell
-        plotter.plotField(pa.createField(MS.X, optargsh={'bins': 100, 'shape': 0}, title='3ppc_order0', rangex=(0,1)), **plotargs)
-        plotter.plotField(pa.createField(MS.X, optargsh={'bins': 100, 'shape': 1}, title='3ppc_order1', rangex=(0,1)), **plotargs)
-        plotter.plotField(pa.createField(MS.X, optargsh={'bins': 100, 'shape': 2}, title='3ppc_order2', rangex=(0,1)), **plotargs)
-
-        # 10 particles per cell
-        plotter.plotField(pa.createField(MS.X, optargsh={'bins': 30, 'shape': 0}, title='10ppc_order0', rangex=(0,1)), **plotargs)
-        plotter.plotField(pa.createField(MS.X, optargsh={'bins': 30, 'shape': 1}, title='10ppc_order1', rangex=(0,1)), **plotargs)
-        plotter.plotField(pa.createField(MS.X, optargsh={'bins': 30, 'shape': 2}, title='10ppc_order2', rangex=(0,1)), **plotargs)
+    # postpic will use matplotlib for plotting. Changing matplotlibs backend
+    # to "Agg" makes it possible to save plots without a display attached.
+    # This is necessary to run this example within the "run-tests" script
+    # on travis-ci.
+    import matplotlib; matplotlib.use('Agg')
 
 
-# --- 2D ---
-if True:
-        dr = pp.readDump(300*30, seed=None, randfunc=np.random.random)
+    # choose the dummy reader. This reader will create fake data for testing.
+    pp.chooseCode('dummy')
+
+    # Create a dummy reader with 300 particles, not initialized with a seed and use
+    # uniform distribution
+    dr = pp.readDump(300, seed=None, randfunc=np.random.random)
+    # set and create directory for pictures.
+    savedir = '_examplepictures/'
+    import os
+    if not os.path.exists(savedir):
+        os.mkdir(savedir)
+
+    # initialze the plotter object.
+    # project name will be prepended to all output names
+    plotter = pp.plotting.plottercls(dr, outdir=savedir, autosave=True, project='particleshapedemo')
+
+    # we will need a refrence to the MultiSpecies quite often
+    from postpic import MultiSpecies as MS
+
+    # create MultiSpecies object for every particle species that exists.
+    pas = [MS(dr, s) for s in dr.listSpecies()]
+
+    # --- 1D visualization of particle contributions ---
+
+    def particleshapedemo(shape):
+        import postpic.cythonfunctions as cf
+        import matplotlib.pyplot as plt
+        ptclpos = np.array([4.5, 9.75, 15.0, 20.25])
+        y, edges = cf.histogram(ptclpos, bins=25, range=(0,25), shape=shape)
+        x = np.convolve(edges, [0.5, 0.5], mode='valid')
+        fig = plt.figure()
+        fig.suptitle('ParticleShape: {:s}'.format(str(shape)))
+        ax = fig.add_subplot(111)
+        ax.plot(x,y)
+        ax.set_ylim((0,1))
+        ax.set_xticks(x, minor=True)
+        ax.grid(which='minor')
+        for ix in ptclpos:
+            ax.axvline(x=ix, color='y')
+        fig.savefig(savedir + 'particleshapedemo{:s}.png'.format(str(shape)), dpi=160)
+        plt.close(fig)
+
+    if True:
+        particleshapedemo(0)
+        particleshapedemo(1)
+        particleshapedemo(2)
+
+    # --- 1D ---
+    if True:
+            pa = pas[0]
+            plotargs = {'ylim': (0,1600), 'log10plot': False}
+
+            # 1 particle per cell
+            plotter.plotField(pa.createField(MS.X, optargsh={'bins': 300, 'shape': 0}, title='1ppc_order0', rangex=(0,1)), **plotargs)
+            plotter.plotField(pa.createField(MS.X, optargsh={'bins': 300, 'shape': 1}, title='1ppc_order1', rangex=(0,1)), **plotargs)
+            plotter.plotField(pa.createField(MS.X, optargsh={'bins': 300, 'shape': 2}, title='1ppc_order2', rangex=(0,1)), **plotargs)
+
+            # 3 particles per cell
+            plotter.plotField(pa.createField(MS.X, optargsh={'bins': 100, 'shape': 0}, title='3ppc_order0', rangex=(0,1)), **plotargs)
+            plotter.plotField(pa.createField(MS.X, optargsh={'bins': 100, 'shape': 1}, title='3ppc_order1', rangex=(0,1)), **plotargs)
+            plotter.plotField(pa.createField(MS.X, optargsh={'bins': 100, 'shape': 2}, title='3ppc_order2', rangex=(0,1)), **plotargs)
+
+            # 10 particles per cell
+            plotter.plotField(pa.createField(MS.X, optargsh={'bins': 30, 'shape': 0}, title='10ppc_order0', rangex=(0,1)), **plotargs)
+            plotter.plotField(pa.createField(MS.X, optargsh={'bins': 30, 'shape': 1}, title='10ppc_order1', rangex=(0,1)), **plotargs)
+            plotter.plotField(pa.createField(MS.X, optargsh={'bins': 30, 'shape': 2}, title='10ppc_order2', rangex=(0,1)), **plotargs)
+
+
+    # --- 2D ---
+    if True:
+            dr = pp.readDump(300*30, seed=None, randfunc=np.random.random)
+            pa = MS(dr, dr.listSpecies()[0])
+            plotargs = {'clim': (0,3e4), 'log10plot': False}
+
+            # 1 particle per cell
+            plotter.plotField(pa.createField(MS.X, MS.Y, optargsh={'bins': (300,30), 'shape': 0}, title='1ppc_order0', rangex=(0,1), rangey=(0,1)), **plotargs)
+            plotter.plotField(pa.createField(MS.X, MS.Y, optargsh={'bins': (300,30), 'shape': 1}, title='1ppc_order1', rangex=(0,1), rangey=(0,1)), **plotargs)
+            plotter.plotField(pa.createField(MS.X, MS.Y, optargsh={'bins': (300,30), 'shape': 2}, title='1ppc_order2', rangex=(0,1), rangey=(0,1)), **plotargs)
+
+            # 3 particles per cell
+            plotter.plotField(pa.createField(MS.X, MS.Y, optargsh={'bins': (100,10), 'shape': 0}, title='3ppc_order0', rangex=(0,1), rangey=(0,1)), **plotargs)
+            plotter.plotField(pa.createField(MS.X, MS.Y, optargsh={'bins': (100,10), 'shape': 1}, title='3ppc_order1', rangex=(0,1), rangey=(0,1)), **plotargs)
+            plotter.plotField(pa.createField(MS.X, MS.Y, optargsh={'bins': (100,10), 'shape': 2}, title='3ppc_order2', rangex=(0,1), rangey=(0,1)), **plotargs)
+
+
+    # --- 3D ---
+    if True:
+        dr = pp.readDump(300*30, seed=None, randfunc=np.random.random, dimensions=3)
         pa = MS(dr, dr.listSpecies()[0])
-        plotargs = {'clim': (0,3e4), 'log10plot': False}
+        # just try to create the field. not plotting routines yet
+        f = pa.createField(MS.X, MS.Y, MS.Z, optargsh={'bins': (30,30,10), 'shape': 2}, title='1ppc_order2', rangex=(0,1), rangey=(0,1), rangez=(0,1))
 
-        # 1 particle per cell
-        plotter.plotField(pa.createField(MS.X, MS.Y, optargsh={'bins': (300,30), 'shape': 0}, title='1ppc_order0', rangex=(0,1), rangey=(0,1)), **plotargs)
-        plotter.plotField(pa.createField(MS.X, MS.Y, optargsh={'bins': (300,30), 'shape': 1}, title='1ppc_order1', rangex=(0,1), rangey=(0,1)), **plotargs)
-        plotter.plotField(pa.createField(MS.X, MS.Y, optargsh={'bins': (300,30), 'shape': 2}, title='1ppc_order2', rangex=(0,1), rangey=(0,1)), **plotargs)
-
-        # 3 particles per cell
-        plotter.plotField(pa.createField(MS.X, MS.Y, optargsh={'bins': (100,10), 'shape': 0}, title='3ppc_order0', rangex=(0,1), rangey=(0,1)), **plotargs)
-        plotter.plotField(pa.createField(MS.X, MS.Y, optargsh={'bins': (100,10), 'shape': 1}, title='3ppc_order1', rangex=(0,1), rangey=(0,1)), **plotargs)
-        plotter.plotField(pa.createField(MS.X, MS.Y, optargsh={'bins': (100,10), 'shape': 2}, title='3ppc_order2', rangex=(0,1), rangey=(0,1)), **plotargs)
-
-
-# --- 3D ---
-if True:
-    dr = pp.readDump(300*30, seed=None, randfunc=np.random.random, dimensions=3)
-    pa = MS(dr, dr.listSpecies()[0])
-    # just try to create the field. not plotting routines yet
-    f = pa.createField(MS.X, MS.Y, MS.Z, optargsh={'bins': (30,30,10), 'shape': 2}, title='1ppc_order2', rangex=(0,1), rangey=(0,1), rangez=(0,1))
-
+if __name__=='__main__':
+    main()
 
