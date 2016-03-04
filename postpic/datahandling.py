62a7e691d19face40425a37e3a3fdd9f846cf9e4
Stefan Tietze <stefan.tietze@uni-jena.de>
2016-03-04 14:59:40 +0100

    adding FFT for field object currently, only 2D fields are supported. Retains the functionality lost by refactoring.

--- postpic/datahandling.py
+++ postpic/datahandling.py
@@ -43,6 +43,7 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import numpy as np
 import copy
 from . import helper
+from .helper import PhysicalConstants as pc
 
 __all__ = ['Field', 'Axis']
 
@@ -203,6 +204,43 @@ class Field(object):
         ax = Axis(**kwargs)
         ax.setextent(extent, matrixpts)
         self._addaxisobj(ax)
+
+    def _fft(self, k0, axes = None):
+        '''
+        applies the fast fourier transform (FFT) to the field object and returns a new transformed field.
+        Currently, only 2D fields are supported. Both axes are transformed, but the axis gives by axes
+        is shifted in such a way, that the 0 component is in the middle.
+        The frequencies are given in terms of k0, which has to be defined by the user.
+        '''
+        if k0 == None:
+            raise ValueError('No k0 specified.')
+        if not (self.dimensions == 2):
+            raise ValueError('This function is only available for 2D fields.')
+        rfftaxes = np.roll((0, 1), axes)
+        ret = copy.deepcopy(self)
+        ret.matrix = 0.5 * pc.epsilon0 * abs(np.fft.fftshift(np.fft.rfft2(self.matrix, axes=rfftaxes), axes=axes))**2
+        ret.unit = '?'
+        ret.name = 'FFT of {0}'.format(self.name)
+        # Assuming all axes are spatial (x, y, z) coordinates. This might not be true for all cases, further checks are needed
+        for axid in rfftaxes:
+            ax = ret.axes[axid]
+            #only linear axes can be transformed
+            if ax.islinear():
+                ax.name = r'$k_{0} / k_0$'.format(ax.name)
+                ax.unit = ''
+                dx = ax.grid_node[1]-ax.grid_node[0]
+                freq = np.fft.rfftfreq(len(ax.grid_node), dx)
+                if axid == axes:
+                    freq_extent = np.array([-freq[-1]/2., freq[-1]/2.])
+                else:
+                    freq_extent = np.array([freq[0], freq[-1]])
+                ax.setextent(freq_extent, len(freq))
+            else:
+                raise ValueError('Specified axis is not linear.')
+        return ret
+        
+    def fft(self, k0 = 1, axes=1):
+        return self._fft(k0, axes)
 
     def setaxisobj(self, axis, axisobj):
         '''
