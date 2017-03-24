e1957fda9a84399f433f9d75349f1ca4c2494d0b
Stephan Kuschel <stephan.kuschel@gmail.com>
2017-03-24 03:05:50 +0100

    add more benchmarking for particle sorting

--- examples/time_cythonfunctions.py
+++ examples/time_cythonfunctions.py
@@ -21,6 +21,8 @@
 def main():
     time_histogram()
     time_particlescalars()
+    time_particleidfilter()
+    time_particleidsort()
 
 def time_histogram():
     import timeit
@@ -36,7 +38,7 @@ def time_histogram():
         print('1D, {:d} shape, {:s}: {:0.2e} sec -> factor {:5.2f} faster'.format(shape, ws, tc, tn/tc))
 
     bins = 1000
-    npart = int(4e6)
+    npart = int(1e6)
     print('=== Histogram 1D bins: {:6d}, npart: {:.1e} ==='.format(bins, npart))
     data = np.random.random(npart)
     weights = np.random.random(npart)
@@ -63,7 +65,7 @@ def time_histogram():
         print('2D, {:d} shape, {:s}: {:0.2e} sec -> factor {:5.2f} faster'.format(shape, ws, tc, tn/tc))
 
     bins = (1000,700)
-    npart = int(4e6)
+    npart = int(1e6)
     print('=== Histogram 2D bins: {:6s}, npart: {:.1e} ==='.format(str(bins), npart))
     datax = np.random.rand(npart)
     datay = np.random.rand(npart)
@@ -92,7 +94,7 @@ def time_histogram():
         print('3D, {:d} shape, {:s}: {:0.2e} sec -> factor {:5.2f} faster'.format(shape, ws, tc, tn/tc))
 
     bins = (200,250,300)  # 15e6 Cells
-    npart = int(4e6)
+    npart = int(1e6)
     print('=== Histogram 3D bins: {:6s}, npart: {:.1e} ==='.format(str(bins), npart))
     datax = np.random.rand(npart)
     datay = np.random.rand(npart)
@@ -123,7 +125,7 @@ def time_particlescalars():
     ms2 = pp.MultiSpecies(dr2, 'electron')
     testexprs = ['x', 'x + y + z', 'gamma', 'beta', 'angle_xy', 'angle_xaxis',
                 'sqrt(x**2 + y**2 + z**2)', 'r_xyz',
-                '(gamma > 1) & (angle_xaxis < 0.1) & (r_xyz < 5e-6)']
+                '(gamma > 1.5) & (angle_xaxis < 0.2) & (r_xyz < 2)']
     print('')
     print('calculation times for n million particles, averaged over 3 calculations each...')
     headformat = ' {:2s}  | {:6s} | {:6s} | {:s}'
@@ -133,11 +135,50 @@ def time_particlescalars():
         t = timeit.Timer(lambda: ms(expr))
         tc = t.timeit(number=3)/3.0
         npartmio = len(ms)/1e6
-        print('{:4.2f} | {:6.2f} | {:6.2f} | {}'.format(npartmio, tc*1e3, (tc*1e3)/npartmio, expr))
+        print('{:4.2f} | {:6.2f} | {:6.2f} | "{}"'.format(npartmio, tc*1e3, (tc*1e3)/npartmio, expr))
     for expr in testexprs:
         for ms in (ms1, ms2):
             timeexpr(expr, ms)
 
+def time_particleidfilter():
+    import postpic as pp
+    import numpy as np
+    import timeit
+    pp.chooseCode('dummy')
+    dr = pp.readDump(1e6, dimensions=3)
+    ms = pp.MultiSpecies(dr, 'electron')
+    totpart = len(ms)
+    def timefilter(ms, expr):
+        ms.uncompress()
+        t = timeit.Timer(lambda: ms.filter(expr))
+        tc = t.timeit(number=1)
+        print('npart = {:.1e}, fraction_taken = {:6.2f}%, time = {:6.2f}ms, expr: "{}"' \
+              .format(totpart, len(ms)/totpart*100, tc*1e3, expr))
+    print('')
+    print('time to filter to a fraction of the particles by expression')
+    timefilter(ms, 'x > 0')
+    timefilter(ms, 'gamma > 1.5')
+    timefilter(ms, '(gamma > 1.5) & (angle_xaxis < 0.2) & (r_xyz < 2)')
+
+def time_particleidsort():
+    import postpic as pp
+    import numpy as np
+    import timeit
+    pp.chooseCode('dummy')
+    dr = pp.readDump(1e6, dimensions=3)
+    ms = pp.MultiSpecies(dr, 'electron')
+    totpart = len(ms)
+    def timeid(ms, ids):
+        ms.uncompress()
+        t = timeit.Timer(lambda: ms.compress(ids))
+        tc = t.timeit(number=1)
+        print('npart = {:.1e}, fraction_taken ={:6.2f}%, time ={:6.2f}ms' \
+              .format(totpart, len(ms)/totpart*100, tc*1e3))
+    print('')
+    print('time to take a fraction of the particles by their ids')
+    timeid(ms, np.arange(100))
+    timeid(ms, np.arange(totpart/20))
+    timeid(ms, np.arange(totpart))
 
 if __name__=='__main__':
     main()
