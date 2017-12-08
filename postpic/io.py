f238196b4495506a962786c14c817d7c6955fb93
Stefan Tietze <stefan.tietze@uni-jena.de>
2017-12-08 10:08:28 +0100

    restore python 2.x compatiblity

--- postpic/io.py
+++ postpic/io.py
@@ -192,7 +192,7 @@ def _make_vectors_help(*fields):
     return np.stack((np.ravel(f, order='F') for f in fields), axis=-1)
 
 
-def export_vector_vtk(filename, *fields, name=''):
+def export_vector_vtk(filename, name='', *fields):
     '''
     exports a vector field to a VTK file suitable for viewing in ParaView.
     Exactly three 3D fields are expected, which will form the X, Y and Z component
