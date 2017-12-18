687e542da44c965f13470e36a290a7c8efaa1511
Alexander Blinne <alexander.blinne@uni-jena.de>
2017-12-18 17:43:31 +0100

    Copied license header to scripts

--- conda/create_environment.sh
+++ conda/create_environment.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+#
+# This file is part of postpic.
+#
+# postpic is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# postpic is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with postpic. If not, see <http://www.gnu.org/licenses/>.
+#
+# Copyright Alexander Blinne 2017
 
 # just get the directory that this file resides within and load the common variables
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
