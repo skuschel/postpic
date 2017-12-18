70d93298e5fb2f071c2085c241bcae4978d55e7f
Alexander Blinne <alexander.blinne@uni-jena.de>
2017-12-18 17:38:07 +0100

    Don't need versioneer for testing

--- conda/create_environment.sh
+++ conda/create_environment.sh
@@ -13,7 +13,6 @@ ubuntu1404)
     conda create -y -p "$CONDA_ENVS/$ENV" python=3.4 matplotlib=1.3.1 numpy=1.8.1 scipy=0.13.3 cython=0.20.1 libgfortran=1 pep8 nose sphinx numexpr future
     source activate "$CONDA_ENVS/$ENV"
     pip install recommonmark
-    pip install versioneer
     pip install urllib3
     pip install pyvtk
     source deactivate
@@ -23,7 +22,6 @@ ubuntu1604py2)
     conda create -y -p "$CONDA_ENVS/$ENV" python=2.7 matplotlib=1.5.1 numpy=1.11.0 scipy=0.17.0 cython=0.23.4 pycodestyle nose sphinx numexpr future urllib3 functools32
     source activate "$CONDA_ENVS/$ENV"
     pip install recommonmark
-    pip install versioneer
     pip install pyvtk
     source deactivate
     ;;
@@ -32,7 +30,6 @@ ubuntu1604py3)
     conda create -y -p "$CONDA_ENVS/$ENV" python=3.5 matplotlib=1.5.1 numpy=1.11.0 scipy=0.17.0 cython=0.23.4 pycodestyle nose sphinx numexpr future urllib3
     source activate "$CONDA_ENVS/$ENV"
     pip install recommonmark
-    pip install versioneer
     pip install pyvtk
     source deactivate
     ;;
@@ -41,7 +38,6 @@ default2)
     conda create -y -p "$CONDA_ENVS/$ENV" python=2 matplotlib numpy scipy cython pycodestyle nose sphinx numexpr future urllib3 functools32
     source activate "$CONDA_ENVS/$ENV"
     pip install recommonmark
-    pip install versioneer
     pip install pyvtk
     source deactivate
     ;;
@@ -50,7 +46,6 @@ default3)
     conda create -y -p "$CONDA_ENVS/$ENV" python=3 matplotlib numpy scipy cython pycodestyle nose sphinx numexpr future urllib3
     source activate "$CONDA_ENVS/$ENV"
     pip install recommonmark
-    pip install versioneer
     pip install pyvtk
     source deactivate
     ;;
