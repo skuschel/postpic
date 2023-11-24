b95c31584a0df3bf4837a85a4ab768dd08ed459d
Stephan Kuschel <stephan.kuschel@gmail.com>
2023-11-24 19:52:06 +0100

    update github actions to new version and make tests pass again

--- setup.py
+++ setup.py
@@ -42,7 +42,8 @@ setup(name='postpic',
                         # does not work for python 2
                         'numpy>=1.8', 'numpy>=1.9;python_version<"3.0"',
                         'scipy', 'future', 'urllib3', 'numexpr',
-                        'cython>=0.18', 'functools32;python_version<"3.0"'],
+                        'cython>=0.18', 'functools32;python_version<"3.0"',
+                        'packaging'],
       extras_require = {
         'h5 reader for openPMD support':  ['h5py'],
         'sdf support for EPOCH reader':  ['sdf'],
